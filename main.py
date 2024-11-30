64952b3346103b980396352345a45b54f5e90c67
RickyDoan <47697871+tommydoan@users.noreply.github.com>
2024-11-30 23:13:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ col1, col2, col3 = st.columns(3)
 with col1:
     age = st.number_input("Age", min_value=15, max_value=72, step=1, value=15)
 with col2:
-    number_of_dependants = st.number_input("Number Of Dependants", min_value=0, max_value=5, step=1, value=2)
-with col3:
-    income_usd = st.number_input("Income (USD) Per Month", min_value=0, max_value=1000, step=1, value=700)
+    insurance_plan = st.selectbox("Insurance Plan", ['Input Insurance Plan', 'Gold', 'Silver', 'Bronze'])
 
+with col3:
+    medical_history = st.selectbox("Medical History", ["Input History", 'No Disease',
+                                                       'High blood pressure', 'Thyroid',
+                                                       'High blood pressure & Heart disease', 'Diabetes & Thyroid',
+                                                       'Diabetes', 'Heart disease', 'Diabetes & High blood pressure',
+                                                       'Diabetes & Heart disease'
+                                                       ])
 # Row 2: Insurance Plan, Gender, Region
 col4, col5, col6 = st.columns(3)
 with col4:
-    insurance_plan = st.selectbox("Insurance Plan", ['Input Insurance Plan','Gold', 'Silver', 'Bronze'])
+    number_of_dependants = st.number_input("Number Of Dependants", min_value=0, max_value=5, step=1, value=2)
 with col5:
-    gender = st.selectbox("Gender", ['Male', 'Female'])
+    income_usd = st.number_input("Income (USD) Per Month", min_value=0, max_value=1000, step=1, value=700)
 with col6:
     region = st.selectbox("Region", ['Southeast', 'Northeast', 'Southwest', 'Northwest'])
 
@@ -34,12 +39,7 @@ with col9:
 # Row 4: Medical History, BMI Category, Smoking Status
 col10, col11, col12 = st.columns(3)
 with col10:
-    medical_history = st.selectbox("Medical History", ["Input History",'No Disease',
-                                                       'High blood pressure', 'Thyroid',
-                                                       'High blood pressure & Heart disease', 'Diabetes & Thyroid',
-                                                       'Diabetes', 'Heart disease', 'Diabetes & High blood pressure',
-                                                       'Diabetes & Heart disease'
-                                                       ])
+    gender = st.selectbox("Gender", ['Male', 'Female'])
 with col11:
     bmi_category = st.selectbox("BMI Category",
                                 ['Overweight', 'Normal',  'Obesity', 'Underweight'])
@@ -70,22 +70,31 @@ list_input = {
 
 
 # Display the collected information (Optional)
-if st.button("Submit"):
+if 'first_click' not in st.session_state:
+    st.session_state.first_click = True  # Track first click
 
+if st.button("Submit"):
     input_yet = []
+
+    if insurance_plan == 'Input Insurance Plan':
+        input_yet.append('Please which INSURANCE PLAN would you like?')
     if age == 15:
-        input_yet.append('Age')
+        input_yet.append('And tell me your AGE ?')
     elif medical_history == 'Input History':
-        input_yet.append('Medical History')
-    elif insurance_plan == 'Input Insurance Plan':
-        input_yet.append('Insurance Plan')
+        input_yet.append('And the last : Your MEDICAL HISTORY')
 
     if input_yet:
         if len(input_yet) == 3:
-            st.warning(f"Please fill all the following fields: {input_yet}")
+            st.warning(f"Please fill Insurance Plan, Age, and Medical History.")
         else:
-            st.warning(f"Please fill in the following fields:   {' ,  '.join(input_yet)}.")
+            st.warning(f"{' '.join(input_yet)}")
     else:
+        # Get the prediction result
         call = predict_db(list_input)
         st.success(call)
+
+        # Show the message only the first time
+        if st.session_state.first_click:
+            st.success("Play around with the inputs to see what changes!")
+            st.session_state.first_click = False
 
